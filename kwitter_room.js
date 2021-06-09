1be00e69d8fa009c30096b6f249840cfe36c87e9
akshatranjan <73059364+akshatranjan@users.noreply.github.com>
2021-06-09 16:41:27 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDhjC3b6tw0XBDgvaQJqLVZayNrwVKnP8U",
+    authDomain: "let-s-chat-e5c1e.firebaseapp.com",
+    databaseURL: "https://let-s-chat-e5c1e-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-e5c1e",
+    storageBucket: "let-s-chat-e5c1e.appspot.com",
+    messagingSenderId: "217922179823",
+    appId: "1:217922179823:web:1c42c4e2d8d80ae014c11a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
